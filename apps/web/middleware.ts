3db61ecaf10b682a872fa8fde6482631f4ed868b
carpihogarai <carpihogar.ai@gmail.com>
2026-01-08 04:40:45 -0800

    chore: fix OpenAI domain verification (www)

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -96,5 +96,5 @@ export default function middleware(request: Request) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next|.*\\..*).*)']
+  matcher: ['/((?!api|_next|.*\\..*|\\.well-known).*)']
 };
